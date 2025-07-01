f4b49b35a32025ef18cc63731e9ecf32a04f03e3
Calvin La <calvin.la@clover.com>
2025-07-01 13:05:43 -0700

    added fixes to display client

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client'; // Import from 'react-dom/client'
+import './styles/main.css'; // Make sure this path is correct if you moved it
 import App from './App';
-import './styles/main.css';
+import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
 
-ReactDOM.render(
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
+  </BrowserRouter>
 );
+
